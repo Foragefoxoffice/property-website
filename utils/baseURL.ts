0a385917fd2d28f384b3e4f900e1163f82ec7609
Foragefoxoffice <forgefoxoffice@gmail.com>
2026-04-29 22:39:54 +0530

    refactor: simplify getBaseURL to use the environment variable directly

--- utils/baseURL.ts
+++ utils/baseURL.ts
@@ -1,7 +1,5 @@
 export const getBaseURL = () => {
-  return typeof window === 'undefined'
-    ? 'http://127.0.0.1:5000/api/v1'
-    : (process.env.NEXT_PUBLIC_API_URL || 'https://183housingsolutions.com/api/v1')
+  return process.env.NEXT_PUBLIC_API_URL || 'https://183housingsolutions.com/api/v1'
 }
 
 export const getAssetBaseURL = () => {
