19120d01709e8c9f67fa015252ead4e6840cc9a8
Foragefoxoffice <forgefoxoffice@gmail.com>
2026-04-29 23:02:05 +0530

    refactor: standardize base URL configuration for server-side and client-side environments

--- utils/baseURL.ts
+++ utils/baseURL.ts
@@ -1,5 +1,10 @@
+export const baseUrl =
+  typeof window === "undefined"
+    ? "http://127.0.0.1:5000/api/v1"
+    : "/api/v1";
+
 export const getBaseURL = () => {
-  return process.env.NEXT_PUBLIC_API_URL || 'https://183housingsolutions.com/api/v1'
+  return baseUrl;
 }
 
 export const getAssetBaseURL = () => {
